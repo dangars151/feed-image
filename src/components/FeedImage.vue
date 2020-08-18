cb1b67b49b7047f4d4d1f94916887da1bff0d6a6
Dang <dang@Dangs-iMac-Pro.local>
2020-08-18 14:29:49 +0700

    test

--- src/components/FeedImage.vue
+++ src/components/FeedImage.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
-    <div v-for="(item, $index) in list" :key="$index">{{ item }}</div>
-
+    <div class="row">
+      <div
+        class="card col-2"
+        v-for="(item, $index) in list"
+        :key="$index"
+        style="margin-bottom: 30px"
+      >
+        <img :src="item.image" :alt="item.description" />
+        <div class="card-body">
+          <router-link to="author">{{ item.username }}</router-link>
+          <br />
+          <router-link to="detail" class="btn btn-primary">Detail</router-link>
+        </div>
+      </div>
+    </div>
     <infinite-loading @infinite="infiniteHandler"></infinite-loading>
   </div>
 </template>
@@ -11,8 +24,7 @@ import axios from "axios";
 import InfiniteLoading from "vue-infinite-loading";
 //const api = "//hn.algolia.com/api/v1/search_by_date?tags=story";
 //const api = "https://secure-caverns-59009.herokuapp.com/photos?all=true";
-const api = "http://192.168.1.120:1337/photos?all=true";
-
+//const api = "http://192.168.1.120:2020/photos";
 export default {
   name: "FeedImage",
   data() {
@@ -24,16 +36,18 @@ export default {
   components: {
     InfiniteLoading,
   },
+  mounted() {
+    if (this.$route.query.page) {
+      this.page = parseInt(this.$route.query.page) - 1;
+    }
+  },
   methods: {
     infiniteHandler($state) {
       axios
-        .get(api, {
-          query: {
-            page: this.page,
-          },
-        })
+        .get(`http://192.168.1.120:2020/photos?page=${this.page}`)
         .then(({ data }) => {
           if (data.length) {
+            this.$router.push({ query: { page: this.page } });
             this.page += 1;
             this.list.push(...data);
             $state.loaded();
@@ -48,7 +62,8 @@ export default {
 
 <style>
 img {
-  width: 300px;
+  width: 90%;
   height: 300px;
+  padding-left: 20px;
 }
 </style>
